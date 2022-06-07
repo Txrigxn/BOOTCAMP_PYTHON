ca7c1770ff30d00b2766db52c469301c13ea0174
Txrigxn <96521135+Txrigxn@users.noreply.github.com>
2022-06-07 11:23:03 +0200

    Initial commit

---
+++ PROJET_2/main.py
@@ -0,0 +1,14 @@
+a = b = ""
+
+
+while not (a.isdigit() and b.isdigit()):
+    a = input("Veuillez entrer un premier nombre :")
+
+    b = input("Veuillez entrer un deuxième nombre:")
+
+    if not (a.isdigit() and b.isdigit()):
+
+        print("Veuillez entrer deux nombres valides")
+
+
+print(f"Le résultat de l'addition du nombre {a} avec le nombre {b} est égal à {int(a) + int(b)}.")
