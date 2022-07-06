1e94b77f8fa9f46a2d7b30daeb39abda1fe4fedb
Txrigxn <96521135+Txrigxn@users.noreply.github.com>
2022-07-06 15:03:29 +0200

    Update main.py

--- PROJET_1/main.py
+++ PROJET_1/main.py
@@ -1,3 +1,5 @@
+#PROJET 1 : CALCULATRICE 
+
 premier_nombre = int(input("Veuillez entrer un premier nombre :"))
 deuxieme_nombre = int(input("Veuillez entrer un deuxième nombre:"))
 print(f"Le résultat de l'addition du nombre {premier_nombre} avec le nombre {deuxieme_nombre} est égal à {premier_nombre + deuxieme_nombre}.")
